c143eaca2ae92d64936942b8d14574460cce2306
Bhargav <bhargavdash@gmail.com>
2026-06-10 17:44:03 +0530

    feat: integrated shadcn & added button component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
+import { Button } from "@/components/ui/button"
 
 function App() {
-
   return (
-    <>
-      <div className="text-2xl font-bold">Hello world!!</div>
-    </>
+    <div className="flex min-h-svh flex-col items-center justify-center">
+      <Button>Click me</Button>
+    </div>
   )
 }
 
